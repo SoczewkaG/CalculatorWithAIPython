a358f6099baebbe865c444bb18ce24dc3fffa465
SoczewkaG <norbig1004@gmail.com>
2026-03-05 16:21:17 +0100

    dodanie funkcji glownie z biblioteki math

---
+++ Calculator/core/scientific.py
@@ -0,0 +1,96 @@
+import math
+
+
+"""
+TODO 
+sinh(x)
+cosh(x)
+tanh(x)
+"""
+def sin(a: float) -> float:
+    return math.sin(a)
+
+
+
+def cos(a: float) -> float:
+    return math.cos(a)
+
+
+
+def tan(a: float) -> float:
+    return math.tan(a)
+
+
+
+def asin(a: float) -> float:
+    return math.asin(a)
+
+
+
+def acos(a: float) -> float:
+    return math.acos(a)
+
+
+
+def atan(a: float) -> float:
+    return math.atan(a)
+
+
+
+def log10(a: float) -> float:
+    return math.log10(a)
+
+
+
+def logn(a: float) -> float:
+    return math.log(a)
+
+
+
+def logx(a: float, b: float) -> float:
+    return math.log(b, a)
+
+
+
+def eX(a: float) -> float:
+    return math.exp(a)
+
+
+
+def e() -> float:
+    return math.e
+
+
+
+def pi() -> float:
+    return math.pi
+
+
+
+def floor(a: float) -> float:
+    return math.floor(a)
+
+
+
+def ceil(a: float) -> float:
+    return math.ceil(a)
+
+
+
+def roundA(a: float) -> float:
+    return round(a)
+
+
+
+def sinh(a: float) -> float:
+    return math.sinh(a)
+
+
+
+def cosh(a: float) -> float:
+    return math.cosh(a)
+
+
+
+def tanh(a: float) -> float:
+    return math.tanh(a)
